729f43cc15d55aa3b3bcaf36bb6a0ab52e6869f7
pglekshmi <pglekshmi88@gmail.com>
2025-02-13 15:09:44 +0530

    updated

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,24 @@
 import { Router } from 'express';
+import {ethers} from 'ethers';
+import artifacts from './artifacts.json' assert{type:"json"}
+
 const router = Router();
 let result;
+const provider =new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+const signer = await provider.getSigner();
+console.log(signer.address);
+const abi = artifacts.abi;
+console.log(abi);
+const address = artifacts.ContractAddress;
+console.log(address);
+const contractInstance = new ethers.Contract(address,abi,signer);
+
 router.get("/", function (req, res) { res.send("Welcome to Backend") })
-router.post('/create',function(req,res){
+router.post('/create',async function(req,res){
     const data=req.body;
     try{
-        result=data;
+        console.log(data);
+        const response = await contractInstance.store(data.input)
         res.status(201).json({msg:"Data Saved"})
     }
     catch (error){
@@ -13,9 +26,11 @@ router.post('/create',function(req,res){
     }
 })
 
-router.get('/read',function(req,res){
+router.get('/read',async function(req,res){
         try{
-    res.status(200).json(result);
+        result=await  contractInstance.retrieve()
+        console.log(result);
+    res.status(200).json({input:Number(result)} );
         }
         catch(error)
         {
